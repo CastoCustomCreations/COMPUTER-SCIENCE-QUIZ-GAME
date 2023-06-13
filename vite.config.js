c0f3444aa2824ee4c585df4fec58eb520f53c0e8
Jen Casto <CastoCustomCreations@gmail.com>
2023-06-13 08:42:01 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/COMPUTER-SCIENCE-QUIZ-GAME/",
+  base: "/Quizzical-React.js-GAME/",
   plugins: [react()],
 });
